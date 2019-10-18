0950875c7fb1e5284761d029c5e59602136ef1e4
Unknown <diyobe2207@gmail.com>
2019-10-18 12:30:24 +0200

    Character behaviour quasi fini
    (L'interaction avec une console n'est pas au point)

---
+++ Assets/Scripts/Clarence Scripts/InteractItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractItem : MonoBehaviour
+{
+
+    public GameObject QTESprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        QTESprite.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void StartQTE()
+    {
+        QTESprite.SetActive(true);
+    }
+
+    public void EndQTE()
+    {
+        QTESprite.SetActive(false);
+    }
+}
