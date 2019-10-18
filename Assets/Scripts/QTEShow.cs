067798113f4b5c6259bd5ae398cb5bedb274e3d2
Abdelkader VANDAMME <vandammeabdel@gmail.com>
2019-10-18 10:25:33 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/QTEShow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTEShow : MonoBehaviour
+{
+
+    public GameObject QTEInput;
+    public GameObject PassFailTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent(QTESystem).enable = false;
+        QTEInput.SetActive(false);
+        PassFailTxt.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            QTEInput.SetActive(true);
+            PassFailTxt.SetActive(true);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            QTEInput.SetActive(false);
+            PassFailTxt.SetActive(false);
+        }
+    }
+}
