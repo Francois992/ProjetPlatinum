e6f79617cd1c882c22ea757075ed2a1017c7ae0e
François <dessartsfran@gmail.com>
2019-10-18 17:34:11 +0200

    Mech underwater movement

---
+++ Assets/Scripts/MechaController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class MechaController : MonoBehaviour
+{
+    private Player _mainPlayer;
+
+    public MechaManager mecha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _mainPlayer = ReInput.players.GetPlayer("PlayerMecha");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dirX = _mainPlayer.GetAxis("MoveHorizontal");
+        float dirY = _mainPlayer.GetAxis("MoveVertical");
+
+        mecha.HorizontalMovement(dirX);
+        mecha.VerticalMovement(dirY);
+    }
+}
