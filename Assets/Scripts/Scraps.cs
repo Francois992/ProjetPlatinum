d75bf54148a89befefa2361bf51e6b6de8ff9559
François <dessartsfran@gmail.com>
2019-12-05 15:06:23 +0100

    Scraps item

--- Assets/Scripts/Scraps.cs
+++ Assets/Scripts/Scraps.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Scraps : MonoBehaviour
 {
+    [SerializeField] private int scrapValue;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter(Collision collision)
     {
-        
+
+        if (collision.gameObject.tag == "Mecha")
+        {
+            Explode();
+        }
+        if (collision.gameObject.tag == "Bullet")
+        {
+            Explode();
+        }
+    }
+
+    private void Explode()
+    {
+        Destroy(gameObject);
+        UIManager.instance.ChangeInventory("Add", ref UIManager.instance.scrapsAmount, scrapValue);
     }
 }
